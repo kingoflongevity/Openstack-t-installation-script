3143de58ae4c69c2dac186a853254caea042b093
Dong hua <62938174+kingoflongevity@users.noreply.github.com>
2021-02-08 16:25:11 +0800

    Create base-compute.sh

---
+++ compute/base-compute.sh
@@ -0,0 +1,14 @@
+#！bin/bash
+yum install epel-release -y
+yum install crudini expect net-tools vim -y
+yum install ntpdate -y
+ntpdate ntp1.aliyun.com
+yum install chrony -y
+source /root/compute/env.sh
+sed -i 's/^server/#&/g' /etc/chrony.conf
+sed -i '7iserver controller iburst' /etc/chrony.conf
+systemctl enable chronyd.service && systemctl start chronyd.service
+yum install centos-release-openstack-train -y
+yum upgrade -y
+yum install python-openstackclient -y
+yum install openstack-selinux -y
