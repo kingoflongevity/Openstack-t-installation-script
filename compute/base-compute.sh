463e94ce24fdc65a7326d3112198e755cace3785
Dong hua <62938174+kingoflongevity@users.noreply.github.com>
2021-02-10 16:53:24 +0800

    Update base-compute.sh

--- compute/base-compute.sh
+++ compute/base-compute.sh
@@ -6,7 +6,7 @@ yum install crudini expect net-tools vim -y
 yum install ntpdate -y
 ntpdate ntp1.aliyun.com
 yum install chrony -y
-source /root/compute/env.sh
+source /root/compute/evn.sh
 sed -i 's/^server/#&/g' /etc/chrony.conf
 sed -i '7iserver controller iburst' /etc/chrony.conf
 systemctl enable chronyd.service && systemctl start chronyd.service
